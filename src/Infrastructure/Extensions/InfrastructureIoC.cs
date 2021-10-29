28a54e1ee0c282e04467c7a7a2e3a781b984e731
Damian Dorocicz <damiand44444@gmail.com>
2021-10-29 23:17:24 +0200

    Setup service

---
+++ src/Infrastructure/Extensions/InfrastructureIoC.cs
@@ -0,0 +1,37 @@
+﻿using Application.Commons.CQRS.Command;
+using Application.Commons.CQRS.Queries;
+using Application.Commons.Persistance;
+using Infrastructure.CQRS.Command;
+using Infrastructure.CQRS.Queries;
+using Infrastructure.Persistance;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Extensions
+{
+    public static class InfrastructureIoC
+    {
+        public static void InjectInfrastructureIoC(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DataContext>(opt => 
+            { 
+                opt.UseNpgsql(configuration.GetConnectionString("DefaultConnection"), 
+                    npgCfg => npgCfg.MigrationsAssembly("Infrastructure"));
+                opt.EnableDetailedErrors();
+            });
+            
+            services.Scan(scan => scan.FromApplicationDependencies()
+                .AddClasses(classes =>classes.AssignableTo(typeof(ICommandHandler<>)))
+                    .AsImplementedInterfaces()
+                    .WithTransientLifetime()
+                .AddClasses(classes => classes.AssignableTo(typeof(IQueryHandler<,>)))
+                    .AsImplementedInterfaces()
+                    .WithTransientLifetime());
+
+            services.AddSingleton<ICommandDispatcher, CommandDispatcher>();
+            services.AddSingleton<IQueryDispatcher, QueryDispatcher>();
+            services.AddScoped<IDataContext, DataContext>();
+        }
+    }
+}
